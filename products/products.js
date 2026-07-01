186f9dcb8804fa39ee309610144e0de6f60b2bb9
apnapinddigital2026 <razsekhon5@gmail.com>
2026-07-01 23:39:05 +0530

    Add product listings for suits and handbags
    Added multiple products with names, prices, and images.

--- products/products.js
+++ products/products.js
@@ -1,1 +1,63 @@
+const products = [
 
+{
+name:"Ladies Unstitched Suit",
+price:999,
+image:"IMG-20260628-WA0000.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1099,
+image:"IMG-20260628-WA0001.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1199,
+image:"IMG-20260628-WA0002.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:999,
+image:"IMG-20260628-WA0003.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1299,
+image:"IMG-20260628-WA0004.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:799,
+image:"IMG-20260628-WA0005.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:899,
+image:"IMG-20260628-WA0006.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:999,
+image:"IMG-20260628-WA0007.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1099,
+image:"IMG-20260628-WA0008.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1199,
+image:"IMG-20260628-WA0009.jpg"
+}
+
+];
