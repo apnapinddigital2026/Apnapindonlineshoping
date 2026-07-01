314f55742ec3f79055600b46e7427ccfd78a8e87
apnapinddigital2026 <razsekhon5@gmail.com>
2026-07-02 00:03:37 +0530

    Update products.js

--- products/products.js
+++ products/products.js
@@ -60,4 +60,1402 @@ price:1199,
 image:"IMG-20260628-WA0009.jpg"
 }
 
-];
+,
+{
+name:"Ladies Hand Bag",
+price:1299,
+image:"IMG-20260628-WA0010.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:499,
+image:"IMG-20260628-WA0011.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:549,
+image:"IMG-20260628-WA0012.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:599,
+image:"IMG-20260628-WA0013.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:649,
+image:"IMG-20260628-WA0014.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1399,
+image:"IMG-20260628-WA0016.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1499,
+image:"IMG-20260628-WA0017.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:999,
+image:"IMG-20260628-WA0019.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1099,
+image:"IMG-20260628-WA0020.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1199,
+image:"IMG-20260628-WA0021.jpg"
+}
+
+ ,
+{
+name:"Ladies Unstitched Suit",
+price:1599,
+image:"IMG-20260628-WA0022.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1499,
+image:"IMG-20260628-WA0023.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1399,
+image:"IMG-20260628-WA0024.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1299,
+image:"IMG-20260628-WA0025.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1199,
+image:"IMG-20260628-WA0026.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:999,
+image:"IMG-20260628-WA0027.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1099,
+image:"IMG-20260628-WA0028.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1199,
+image:"IMG-20260628-WA0029.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:599,
+image:"IMG-20260628-WA0030.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:699,
+image:"IMG-20260628-WA0031.jpg"
+}
+
+ ,
+{
+name:"Gents T-Shirt",
+price:749,
+image:"IMG-20260628-WA0032.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:799,
+image:"IMG-20260628-WA0033.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:849,
+image:"IMG-20260628-WA0034.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1299,
+image:"IMG-20260628-WA0035.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1399,
+image:"IMG-20260628-WA0036.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1699,
+image:"IMG-20260628-WA0037.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1799,
+image:"IMG-20260628-WA0038.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1899,
+image:"IMG-20260628-WA0039.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1999,
+image:"IMG-20260628-WA0040.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2099,
+image:"IMG-20260628-WA0041.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:2199,
+image:"IMG-20260628-WA0042.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2299,
+image:"IMG-20260628-WA0043.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2399,
+image:"IMG-20260628-WA0044.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2499,
+image:"IMG-20260628-WA0045.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2599,
+image:"IMG-20260628-WA0046.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1499,
+image:"IMG-20260628-WA0047.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1599,
+image:"IMG-20260628-WA0048.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1699,
+image:"IMG-20260628-WA0049.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:899,
+image:"IMG-20260628-WA0050.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:999,
+image:"IMG-20260628-WA0051.jpg"
+}
+
+,
+{
+name:"Gents T-Shirt",
+price:1099,
+image:"IMG-20260628-WA0052.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1199,
+image:"IMG-20260628-WA0053.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1299,
+image:"IMG-20260628-WA0054.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1799,
+image:"IMG-20260628-WA0055.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1899,
+image:"IMG-20260628-WA0056.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1999,
+image:"IMG-20260628-WA0057.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2699,
+image:"IMG-20260628-WA0058.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2799,
+image:"IMG-20260628-WA0059.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2899,
+image:"IMG-20260628-WA0060.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2999,
+image:"IMG-20260628-WA0061.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:3099,
+image:"IMG-20260628-WA0062.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3199,
+image:"IMG-20260628-WA0063.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3299,
+image:"IMG-20260628-WA0064.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3399,
+image:"IMG-20260628-WA0065.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2099,
+image:"IMG-20260628-WA0066.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2199,
+image:"IMG-20260628-WA0067.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2299,
+image:"IMG-20260628-WA0068.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1399,
+image:"IMG-20260628-WA0069.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1499,
+image:"IMG-20260628-WA0070.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1599,
+image:"IMG-20260628-WA0071.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:3499,
+image:"IMG-20260628-WA0072.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3599,
+image:"IMG-20260628-WA0073.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3699,
+image:"IMG-20260628-WA0074.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3799,
+image:"IMG-20260628-WA0075.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2399,
+image:"IMG-20260628-WA0076.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2499,
+image:"IMG-20260628-WA0077.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2599,
+image:"IMG-20260628-WA0078.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1699,
+image:"IMG-20260628-WA0079.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1799,
+image:"IMG-20260628-WA0080.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1899,
+image:"IMG-20260628-WA0081.jpg"
+}
+
+ ,
+{
+name:"Ladies Unstitched Suit",
+price:3899,
+image:"IMG-20260628-WA0082.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3999,
+image:"IMG-20260628-WA0083.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4099,
+image:"IMG-20260628-WA0084.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4199,
+image:"IMG-20260628-WA0085.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2699,
+image:"IMG-20260628-WA0086.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2799,
+image:"IMG-20260628-WA0087.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2899,
+image:"IMG-20260628-WA0088.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1999,
+image:"IMG-20260628-WA0089.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2099,
+image:"IMG-20260628-WA0090.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2199,
+image:"IMG-20260628-WA0091.jpg"
+}
+
+ ,
+{
+name:"Ladies Unstitched Suit",
+price:4299,
+image:"IMG-20260628-WA0092.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4399,
+image:"IMG-20260628-WA0093.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4499,
+image:"IMG-20260628-WA0094.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4599,
+image:"IMG-20260628-WA0095.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2999,
+image:"IMG-20260628-WA0096.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3099,
+image:"IMG-20260628-WA0097.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3199,
+image:"IMG-20260628-WA0098.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2299,
+image:"IMG-20260628-WA0099.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2399,
+image:"IMG-20260628-WA0100.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2499,
+image:"IMG-20260628-WA0101.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:4699,
+image:"IMG-20260628-WA0102.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4799,
+image:"IMG-20260628-WA0103.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4899,
+image:"IMG-20260628-WA0104.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4999,
+image:"IMG-20260628-WA0105.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3299,
+image:"IMG-20260628-WA0106.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3399,
+image:"IMG-20260628-WA0107.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3499,
+image:"IMG-20260628-WA0108.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2599,
+image:"IMG-20260628-WA0109.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2699,
+image:"IMG-20260628-WA0110.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2799,
+image:"IMG-20260628-WA0111.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:2899,
+image:"IMG-20260628-WA0112.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2999,
+image:"IMG-20260628-WA0113.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3099,
+image:"IMG-20260628-WA0114.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3199,
+image:"IMG-20260628-WA0115.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1599,
+image:"IMG-20260628-WA0116.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1699,
+image:"IMG-20260628-WA0117.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1799,
+image:"IMG-20260628-WA0118.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:899,
+image:"IMG-20260628-WA0119.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:999,
+image:"IMG-20260628-WA0120.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1099,
+image:"IMG-20260628-WA0121.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:1199,
+image:"IMG-20260628-WA0122.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1299,
+image:"IMG-20260628-WA0123.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1399,
+image:"IMG-20260628-WA0124.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1499,
+image:"IMG-20260628-WA0125.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1899,
+image:"IMG-20260628-WA0126.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:1999,
+image:"IMG-20260628-WA0127.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2099,
+image:"IMG-20260628-WA0128.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1199,
+image:"IMG-20260628
+,
+{
+name:"Ladies Unstitched Suit",
+price:1499,
+image:"IMG-20260628-WA0132.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1599,
+image:"IMG-20260628-WA0133.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1699,
+image:"IMG-20260628-WA0134.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:1799,
+image:"IMG-20260628-WA0135.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2199,
+image:"IMG-20260628-WA0136.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2299,
+image:"IMG-20260628-WA0137.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2399,
+image:"IMG-20260628-WA0138.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1499,
+image:"IMG-20260628-WA0139.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1599,
+image:"IMG-20260628-WA0140.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:1699,
+image:"IMG-20260628-WA0141.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:2299,
+image:"IMG-20260628-WA0152.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2399,
+image:"IMG-20260628-WA0153.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2499,
+image:"IMG-20260628-WA0154.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2599,
+image:"IMG-20260628-WA0155.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2799,
+image:"IMG-20260628-WA0156.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2899,
+image:"IMG-20260628-WA0157.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:2999,
+image:"IMG-20260628-WA0158.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2099,
+image:"IMG-20260628-WA0159.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2199,
+image:"IMG-20260628-WA0160.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2299,
+image:"IMG-20260628-WA0161.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:2699,
+image:"IMG-20260628-WA0162.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2799,
+image:"IMG-20260628-WA0163.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2899,
+image:"IMG-20260628-WA0164.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:2999,
+image:"IMG-20260628-WA0165.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3099,
+image:"IMG-20260628-WA0166.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3199,
+image:"IMG-20260628-WA0167.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3299,
+image:"IMG-20260628-WA0168.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2399,
+image:"IMG-20260628-WA0169.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2499,
+image:"IMG-20260628-WA0170.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2599,
+image:"IMG-20260628-WA0171.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:3099,
+image:"IMG-20260628-WA0172.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3199,
+image:"IMG-20260628-WA0173.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3299,
+image:"IMG-20260628-WA0174.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3399,
+image:"IMG-20260628-WA0175.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3399,
+image:"IMG-20260628-WA0176.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3499,
+image:"IMG-20260628-WA0177.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3599,
+image:"IMG-20260628-WA0178.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2699,
+image:"IMG-20260628-WA0179.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2799,
+image:"IMG-20260628-WA0180.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:2899,
+image:"IMG-20260628-WA0181.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:3899,
+image:"IMG-20260628-WA0192.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:3999,
+image:"IMG-20260628-WA0193.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4099,
+image:"IMG-20260628-WA0194.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4199,
+image:"IMG-20260628-WA0195.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:3999,
+image:"IMG-20260628-WA0196.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:4099,
+image:"IMG-20260628-WA0197.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:4199,
+image:"IMG-20260628-WA0198.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:3299,
+image:"IMG-20260628-WA0199.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:3399,
+image:"IMG-20260628-WA0200.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:3499,
+image:"IMG-20260628-WA0201.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:4299,
+image:"IMG-20260628-WA0202.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4399,
+image:"IMG-20260628-WA0203.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4499,
+image:"IMG-20260628-WA0204.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4599,
+image:"IMG-20260628-WA0205.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:4299,
+image:"IMG-20260628-WA0206.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:4399,
+image:"IMG-20260628-WA0207.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:4499,
+image:"IMG-20260628-WA0208.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:3599,
+image:"IMG-20260628-WA0209.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:3699,
+image:"IMG-20260628-WA0210.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:3799,
+image:"IMG-20260628-WA0211.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:4699,
+image:"IMG-20260628-WA0212.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4799,
+image:"IMG-20260628-WA0213.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4899,
+image:"IMG-20260628-WA0214.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:4999,
+image:"IMG-20260628-WA0215.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:4599,
+image:"IMG-20260628-WA0216.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:4699,
+image:"IMG-20260628-WA0217.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:4799,
+image:"IMG-20260628-WA0218.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:3899,
+image:"IMG-20260628-WA0219.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:3999,
+image:"IMG-20260628-WA0220.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:4099,
+image:"IMG-20260628-WA0221.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:5099,
+image:"IMG-20260628-WA0222.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:5199,
+image:"IMG-20260628-WA0223.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:5299,
+image:"IMG-20260628-WA0224.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:5399,
+image:"IMG-20260628-WA0225.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:4899,
+image:"IMG-20260628-WA0226.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:4999,
+image:"IMG-20260628-WA0227.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:5099,
+image:"IMG-20260628-WA0228.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:4199,
+image:"IMG-20260628-WA0229.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:4299,
+image:"IMG-20260628-WA0230.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:4399,
+image:"IMG-20260628-WA0231.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:5499,
+image:"IMG-20260628-WA0232.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:5599,
+image:"IMG-20260628-WA0233.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:5699,
+image:"IMG-20260628-WA0234.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:5799,
+image:"IMG-20260628-WA0235.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:5199,
+image:"IMG-20260628-WA0236.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:5299,
+image:"IMG-20260628-WA0237.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:5399,
+image:"IMG-20260628-WA0238.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:4499,
+image:"IMG-20260628-WA0239.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:4599,
+image:"IMG-20260628-WA0240.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:4699,
+image:"IMG-20260628-WA0241.jpg"
+}
+
+,
+{
+name:"Ladies Unstitched Suit",
+price:5899,
+image:"IMG-20260628-WA0242.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:5999,
+image:"IMG-20260628-WA0243.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:6099,
+image:"IMG-20260628-WA0244.jpg"
+},
+
+{
+name:"Ladies Unstitched Suit",
+price:6199,
+image:"IMG-20260628-WA0245.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:5499,
+image:"IMG-20260628-WA0246.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:5599,
+image:"IMG-20260628-WA0247.jpg"
+},
+
+{
+name:"Ladies Hand Bag",
+price:5699,
+image:"IMG-20260628-WA0248.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:4799,
+image:"IMG-20260628-WA0249.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:4899,
+image:"IMG-20260628-WA0250.jpg"
+},
+
+{
+name:"Gents T-Shirt",
+price:4999,
+image:"IMG-20260628-WA0251.jpg"
+}
+// Products Array (products.js ਤੋਂ ਆਵੇਗਾ)
+
+let cart = [];
+
+// ਸਾਰੇ Products ਦਿਖਾਓ
+function loadProducts(list = products){
+
+const container = document.getElementById("products");
+
+if(!container) return;
+
+container.innerHTML = "";
+
+list.forEach((item,index)=>{
+
+container.innerHTML += `
+<div class="card">
+
+<img src="${item.image}" alt="${item.name}">
+
+<h3>${item.name}</h3>
+
+<p><strong>₹${item.price}</strong></p>
+
+<button onclick="addToCart(${index})">
+🛒 Add to Cart
+</button>
+
+<button onclick="whatsappOrder('${item.name}')">
+💬 WhatsApp Order
+</button>
+
+</div>
+`;
+
+// Search Products
+function searchProducts(){
+
+const text = document.getElementById("search").value.toLowerCase();
+
+const filtered = products.filter(item =>
+item.name.toLowerCase().includes(text)
+);
+
+loadProducts(filtered);
+
+}
+
+// Add To Cart
+function addToCart(index){
+
+cart.push(products[index]);
+
+alert(products[index].name + " Cart ਵਿੱਚ ਸ਼ਾਮਲ ਹੋ ਗਿਆ।");
+
+updateCart();
+
+}
+
+// Cart Counter
+function updateCart(){
+
+const cartCount = document.getElementById("cart-count");
+
+if(cartCount){
+cartCount.innerText = cart.length;
+}
+
+// Website Load ਹੋਣ 'ਤੇ Products ਦਿਖਾਓ
+window.onload = function(){
+loadProducts();
+}
