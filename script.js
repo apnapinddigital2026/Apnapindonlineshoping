c8599ef4b4cd6eac0898be3d965c028166b21e4f
apnapinddigital2026 <razsekhon5@gmail.com>
2026-07-02 09:01:29 +0530

    Add product display and search features
    Implement product display and search functionality.

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
+let filteredProducts = [];
 
+window.onload = function () {
+    filteredProducts = products;
+    displayProducts(filteredProducts);
+};
+
+function displayProducts(list) {
+
+    const container = document.getElementById("products");
+
+    container.innerHTML = "";
+
+    list.forEach((product) => {
+
+        container.innerHTML += `
+        <div class="card">
+
+            <img src="${product.image}" alt="${product.name}">
+
+            <h3>${product.name}</h3>
+
+            <p><strong>₹${product.price}</strong></p>
+
+            <button onclick="viewProduct(${product.id})">
+            👁 View Product
+            </button>
+
+        </div>
+        `;
+
+    });
+
+}
+
+function searchProducts() {
+
+    const keyword = document
+        .getElementById("search")
+        .value
+        .toLowerCase();
+
+    filteredProducts = products.filter(product =>
+        product.name.toLowerCase().includes(keyword)
+    );
+
+    displayProducts(filteredProducts);
+
+}
+
+function viewProduct(id) {
+
+    localStorage.setItem("productId", id);
+
+    window.location.href = "product.html";
+
+}
