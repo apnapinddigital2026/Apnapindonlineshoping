3fa5fd91f6e6fb863637246a951ec9236f632151
apnapinddigital2026 <razsekhon5@gmail.com>
2026-07-03 09:54:20 +0530

    Update products.js

--- products/products.js
+++ products/products.js
@@ -55,5 +55,129 @@ price:1599,
 image:"../images/images/ladies-wear/IMG-20260628-WA0013.jpg",
 category:"Ladies Suit"
 }
+,
 
+{
+id:8,
+name:"Ladies Unstitched Suit 01",
+price:1699,
+image:"../images/images/unstitched-suits/IMG-20260628-WA0026.jpg",
+category:"Ladies Unstitched Suit"
+},
+
+{
+id:9,
+name:"Ladies Unstitched Suit 02",
+price:1799,
+image:"../images/images/unstitched-suits/IMG-20260628-WA0027.jpg",
+category:"Ladies Unstitched Suit"
+},
+
+{
+id:10,
+name:"Ladies Unstitched Suit 03",
+price:1899,
+image:"../images/images/unstitched-suits/IMG-20260628-WA0028.jpg",
+category:"Ladies Unstitched Suit"
+},
+
+{
+id:11,
+name:"Ladies Unstitched Suit 04",
+price:1999,
+image:"../images/images/unstitched-suits/IMG-20260628-WA0029.jpg",
+category:"Ladies Unstitched Suit"
+},
+
+{
+id:12,
+name:"Ladies Unstitched Suit 05",
+price:2099,
+image:"../images/images/unstitched-suits/IMG-20260628-WA0030.jpg",
+category:"Ladies Unstitched Suit"
+},
+
+{
+id:13,
+name:"Ladies Unstitched Suit 06",
+price:2199,
+image:"../images/images/unstitched-suits/IMG-20260628-WA0031.jpg",
+category:"Ladies Unstitched Suit"
+},
+
+{
+id:14,
+name:"Ladies Unstitched Suit 07",
+price:2299,
+image:"../images/images/unstitched-suits/IMG-20260628-WA0032.jpg",
+category:"Ladies Unstitched Suit"
+}{
+id:15,
+name:"Ladies Hand Bag 01",
+price:1499,
+image:"../images/images/handbags/IMG-20260628-WA0004.jpg",
+category:"Hand Bag"
+},
+
+{
+id:16,
+name:"Ladies Hand Bag 02",
+price:1599,
+image:"../images/images/handbags/IMG-20260628-WA0005.jpg",
+category:"Hand Bag"
+},
+
+{
+id:17,
+name:"Ladies Hand Bag 03",
+price:1699,
+image:"../images/images/handbags/IMG-20260628-WA0009.jpg",
+category:"Hand Bag"
+},
+
+{
+id:18,
+name:"Ladies Hand Bag Premium",
+price:2199,
+image:"../images/images/handbags/IMG-20260628-WA0010.jpg",
+category:"Hand Bag"
+},{
+id:19,
+name:"Gents T-Shirt 01",
+price:799,
+image:"../images/images/gents-wear/IMG-20260628-WA0006.jpg",
+category:"Gents"
+},
+
+{
+id:20,
+name:"Gents T-Shirt 02",
+price:899,
+image:"../images/images/gents-wear/IMG-20260628-WA0007.jpg",
+category:"Gents"
+},
+
+{
+id:21,
+name:"Gents T-Shirt Premium",
+price:1299,
+image:"../images/images/gents-wear/IMG-20260628-WA0010.jpg",
+category:"Gents"
+},
+
+{
+id:22,
+name:"Gents Shirt 01",
+price:999,
+image:"../images/images/gents-wear/IMG-20260628-WA0014.jpg",
+category:"Gents"
+},
+
+{
+id:23,
+name:"Gents Shirt 02",
+price:1099,
+image:"../images/images/gents-wear/IMG-20260628-WA0015.jpg",
+category:"Gents"
+},
 ];
