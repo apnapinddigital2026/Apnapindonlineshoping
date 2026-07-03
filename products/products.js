9158dff1d02d4816a6245f29f8364b764e9028a4
apnapinddigital2026 <razsekhon5@gmail.com>
2026-07-03 11:29:06 +0530

    Update products.js

--- products/products.js
+++ products/products.js
@@ -4,7 +4,7 @@ const products = [
 id:1,
 name:"Ladies Suit 01",
 price:999,
-image:"../images/images/ladies-wear/IMG-20260628-WA0001.jpg",
+image:"../images/image/ladies wear/IMG-20260628-WA0001.jpg",
 category:"Ladies Suit"
 },
 
@@ -12,7 +12,7 @@ category:"Ladies Suit"
 id:2,
 name:"Ladies Suit 02",
 price:1099,
-image:"../images/images/ladies-wear/IMG-20260628-WA0002.jpg",
+image:"../images/image/ladies wear/IMG-20260628-WA0002.jpg",
 category:"Ladies Suit"
 },
 
@@ -20,7 +20,7 @@ category:"Ladies Suit"
 id:3,
 name:"Ladies Suit 03",
 price:1199,
-image:"../images/images/ladies-wear/IMG-20260628-WA0003.jpg",
+image:"../images/image/ladies wear/IMG-20260628-WA0003.jpg",
 category:"Ladies Suit"
 },
 
@@ -28,7 +28,7 @@ category:"Ladies Suit"
 id:4,
 name:"Ladies Suit 04",
 price:1299,
-image:"../images/images/ladies-wear/IMG-20260628-WA0008.jpg",
+image:"../images/image/ladies wear/IMG-20260628-WA0008.jpg",
 category:"Ladies Suit"
 },
 
@@ -36,7 +36,7 @@ category:"Ladies Suit"
 id:5,
 name:"Ladies Suit 05",
 price:1399,
-image:"../images/images/ladies-wear/IMG-20260628-WA0011.jpg",
+image:"../images/image/ladies wear/IMG-20260628-WA0011.jpg",
 category:"Ladies Suit"
 },
 
@@ -44,7 +44,7 @@ category:"Ladies Suit"
 id:6,
 name:"Ladies Suit 06",
 price:1499,
-image:"../images/images/ladies-wear/IMG-20260628-WA0012.jpg",
+image:"../images/image/ladies wear/IMG-20260628-WA0012.jpg",
 category:"Ladies Suit"
 },
 
@@ -52,16 +52,15 @@ category:"Ladies Suit"
 id:7,
 name:"Ladies Suit 07",
 price:1599,
-image:"../images/images/ladies-wear/IMG-20260628-WA0013.jpg",
+image:"../images/image/ladies wear/IMG-20260628-WA0013.jpg",
 category:"Ladies Suit"
-}
-,
+},
 
 {
 id:8,
 name:"Ladies Unstitched Suit 01",
 price:1699,
-image:"../images/images/unstitched-suits/IMG-20260628-WA0026.jpg",
+image:"../images/image/unstitched suits/IMG-20260628-WA0026.jpg",
 category:"Ladies Unstitched Suit"
 },
 
@@ -69,7 +68,7 @@ category:"Ladies Unstitched Suit"
 id:9,
 name:"Ladies Unstitched Suit 02",
 price:1799,
-image:"../images/images/unstitched-suits/IMG-20260628-WA0027.jpg",
+image:"../images/image/unstitched suits/IMG-20260628-WA0027.jpg",
 category:"Ladies Unstitched Suit"
 },
 
@@ -77,15 +76,13 @@ category:"Ladies Unstitched Suit"
 id:10,
 name:"Ladies Unstitched Suit 03",
 price:1899,
-image:"../images/images/unstitched-suits/IMG-20260628-WA0028.jpg",
+image:"../images/image/unstitched suits/IMG-20260628-WA0028.jpg",
 category:"Ladies Unstitched Suit"
-},
-
-{
+},{
 id:11,
 name:"Ladies Unstitched Suit 04",
 price:1999,
-image:"../images/images/unstitched-suits/IMG-20260628-WA0029.jpg",
+image:"../images/image/unstitched suits/IMG-20260628-WA0029.jpg",
 category:"Ladies Unstitched Suit"
 },
 
@@ -93,7 +90,7 @@ category:"Ladies Unstitched Suit"
 id:12,
 name:"Ladies Unstitched Suit 05",
 price:2099,
-image:"../images/images/unstitched-suits/IMG-20260628-WA0030.jpg",
+image:"../images/image/unstitched suits/IMG-20260628-WA0030.jpg",
 category:"Ladies Unstitched Suit"
 },
 
@@ -101,7 +98,7 @@ category:"Ladies Unstitched Suit"
 id:13,
 name:"Ladies Unstitched Suit 06",
 price:2199,
-image:"../images/images/unstitched-suits/IMG-20260628-WA0031.jpg",
+image:"../images/image/unstitched suits/IMG-20260628-WA0031.jpg",
 category:"Ladies Unstitched Suit"
 },
 
@@ -109,13 +106,15 @@ category:"Ladies Unstitched Suit"
 id:14,
 name:"Ladies Unstitched Suit 07",
 price:2299,
-image:"../images/images/unstitched-suits/IMG-20260628-WA0032.jpg",
+image:"../images/image/unstitched suits/IMG-20260628-WA0032.jpg",
 category:"Ladies Unstitched Suit"
-}{
+},
+
+{
 id:15,
 name:"Ladies Hand Bag 01",
 price:1499,
-image:"../images/images/handbags/IMG-20260628-WA0004.jpg",
+image:"../images/image/handbag/IMG-20260628-WA0004.jpg",
 category:"Hand Bag"
 },
 
@@ -123,7 +122,7 @@ category:"Hand Bag"
 id:16,
 name:"Ladies Hand Bag 02",
 price:1599,
-image:"../images/images/handbags/IMG-20260628-WA0005.jpg",
+image:"../images/image/handbag/IMG-20260628-WA0005.jpg",
 category:"Hand Bag"
 },
 
@@ -131,7 +130,7 @@ category:"Hand Bag"
 id:17,
 name:"Ladies Hand Bag 03",
 price:1699,
-image:"../images/images/handbags/IMG-20260628-WA0009.jpg",
+image:"../images/image/handbag/IMG-20260628-WA0009.jpg",
 category:"Hand Bag"
 },
 
@@ -139,13 +138,13 @@ category:"Hand Bag"
 id:18,
 name:"Ladies Hand Bag Premium",
 price:2199,
-image:"../images/images/handbags/IMG-20260628-WA0010.jpg",
+image:"../images/image/handbag/IMG-20260628-WA0010.jpg",
 category:"Hand Bag"
 },{
 id:19,
 name:"Gents T-Shirt 01",
 price:799,
-image:"../images/images/gents-wear/IMG-20260628-WA0006.jpg",
+image:"../images/image/gents wear/IMG-20260628-WA0006.jpg",
 category:"Gents"
 },
 
@@ -153,7 +152,7 @@ category:"Gents"
 id:20,
 name:"Gents T-Shirt 02",
 price:899,
-image:"../images/images/gents-wear/IMG-20260628-WA0007.jpg",
+image:"../images/image/gents wear/IMG-20260628-WA0007.jpg",
 category:"Gents"
 },
 
@@ -161,7 +160,7 @@ category:"Gents"
 id:21,
 name:"Gents T-Shirt Premium",
 price:1299,
-image:"../images/images/gents-wear/IMG-20260628-WA0010.jpg",
+image:"../images/image/gents wear/IMG-20260628-WA0010.jpg",
 category:"Gents"
 },
 
@@ -169,7 +168,7 @@ category:"Gents"
 id:22,
 name:"Gents Shirt 01",
 price:999,
-image:"../images/images/gents-wear/IMG-20260628-WA0014.jpg",
+image:"../images/image/gents wear/IMG-20260628-WA0014.jpg",
 category:"Gents"
 },
 
@@ -177,7 +176,8 @@ category:"Gents"
 id:23,
 name:"Gents Shirt 02",
 price:1099,
-image:"../images/images/gents-wear/IMG-20260628-WA0015.jpg",
+image:"../images/image/gents wear/IMG-20260628-WA0015.jpg",
 category:"Gents"
-},
+}
+
 ];
