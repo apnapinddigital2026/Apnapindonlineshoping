bb30ce1e8bd0b5ff30cd675f47ee09143333bd11
apnapinddigital2026 <razsekhon5@gmail.com>
2026-07-03 09:29:53 +0530

    Update product entries to ladies suits and images

--- products/products.js
+++ products/products.js
@@ -4,7 +4,7 @@ const products = [
 id:1,
 name:"Ladies Suit 01",
 price:999,
-image:"IMG-20260628-WA0001.jpg",
+image:"../images/images/ladies-wear/IMG-20260628-WA0001.jpg",
 category:"Ladies Suit"
 },
 
@@ -12,7 +12,7 @@ category:"Ladies Suit"
 id:2,
 name:"Ladies Suit 02",
 price:1099,
-image:"IMG-20260628-WA0002.jpg",
+image:"../images/images/ladies-wear/IMG-20260628-WA0002.jpg",
 category:"Ladies Suit"
 },
 
@@ -20,64 +20,40 @@ category:"Ladies Suit"
 id:3,
 name:"Ladies Suit 03",
 price:1199,
-image:"IMG-20260628-WA0003.jpg",
+image:"../images/images/ladies-wear/IMG-20260628-WA0003.jpg",
 category:"Ladies Suit"
 },
 
 {
 id:4,
-name:"Ladies Hand Bag 01",
-price:1499,
-image:"IMG-20260628-WA0004.jpg",
-category:"Hand Bag"
-},
-
-{
-id:5,
-name:"Ladies Hand Bag 02",
-price:1599,
-image:"IMG-20260628-WA0005.jpg",
-category:"Hand Bag"
-},
-
-{
-id:6,
-name:"Gents T-Shirt 01",
-price:799,
-image:"IMG-20260628-WA0006.jpg",
-category:"Gents"
-},
-
-{
-id:7,
-name:"Gents T-Shirt 02",
-price:899,
-image:"IMG-20260628-WA0007.jpg",
-category:"Gents"
-},
-
-{
-id:8,
-name:"Ladies Suit Premium",
-price:1899,
-image:"IMG-20260628-WA0008.jpg",
+name:"Ladies Suit 04",
+price:1299,
+image:"../images/images/ladies-wear/IMG-20260628-WA0008.jpg",
 category:"Ladies Suit"
 },
 
 {
-id:9,
-name:"Ladies Hand Bag Premium",
-price:2199,
-image:"IMG-20260628-WA0009.jpg",
-category:"Hand Bag"
+id:5,
+name:"Ladies Suit 05",
+price:1399,
+image:"../images/images/ladies-wear/IMG-20260628-WA0011.jpg",
+category:"Ladies Suit"
 },
 
 {
-id:10,
-name:"Gents T-Shirt Premium",
-price:1299,
-image:"IMG-20260628-WA0010.jpg",
-category:"Gents"
+id:6,
+name:"Ladies Suit 06",
+price:1499,
+image:"../images/images/ladies-wear/IMG-20260628-WA0012.jpg",
+category:"Ladies Suit"
+},
+
+{
+id:7,
+name:"Ladies Suit 07",
+price:1599,
+image:"../images/images/ladies-wear/IMG-20260628-WA0013.jpg",
+category:"Ladies Suit"
 }
 
 ];
