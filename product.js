dbe2b013d91740c02a762f28bd1d867128338e64
apnapinddigital2026 <razsekhon5@gmail.com>
2026-07-04 17:49:23 +0530

    Update product.js

--- product.js
+++ product.js
@@ -6,81 +6,37 @@
 const products = [
 
 {
-    id:1,
-    name:"Ladies Suit",
-    price:999,
-    category:"ladies",
-    image:"images/ladies/suit1.jpg",
-    description:"Premium Ladies Suit"
+    id: 1,
+    name: "Ladies Suit",
+    price: 999,
+    category: "ladies",
+    image: "images/ladies/suit1.jpg",
+    description: "Premium Ladies Suit"
 },
 
 {
-    id:2,
-    name:"Ladies Suit 2",
-    price:1299,
-    category:"ladies",
-    image:"images/ladies/suit2.jpg",
-    description:"Designer Ladies Suit"
+    id: 2,
+    name: "Ladies Suit 2",
+    price: 1299,
+    category: "ladies",
+    image: "images/ladies/suit2.jpg",
+    description: "Designer Ladies Suit"
 },
 
 {
-    id:3,
-    name:"Ladies Handbag",
-    price:1499,
-    category:"ladies-handbags",
-    image:"images/ladies-handbags/handbag-01.jpg",
-    description:"Stylish Ladies Handbag"
+    id: 3,
+    name: "Ladies Handbag",
+    price: 1499,
+    category: "ladies-handbags",
+    image: "images/ladies-handbags/handbag-01.jpg",
+    description: "Stylish Ladies Handbag"
 },
 
 {
-    id:4,
-    name:"Gents Shirt",
-    price:799,
-    category:"gents",
-    image:"images/gents/shirt1.jpg",
-    description:"Premium Cotton Shirt"
+    id: 4,
+    name: "Gents Shirt",
+    price: 799,
+    category: "gents",
+    image: "images/gents/shirt1.jpg",
+    description: "Premium Cotton Shirt"
 },
-
-{
-    id:5,
-    name:"Kids Wear",
-    price:699,
-    category:"kids",
-    image:"images/kids/kids1.jpg",
-    description:"Comfortable Kids Wear"
-},
-
-{
-    id:6,
-    name:"Sports Shoes",
-    price:1799,
-    category:"footwear",
-    image:"images/footwear/sh
-{
-    id:7,
-    name:"Ladies Fashion Suit",
-    price:1499,
-    category:"ladies",
-    image:"images/ladies/IMG-20260628-WA0033.jpg",
-    description:"Beautiful Ladies Suit"
-},
-
-{
-    id:8,
-    name:"Ladies Handbag",
-    price:999,
-    category:"ladies-handbags",
-    image:"images/ladies-handbags/IMG-20260628-WA0121.jpg",
-    description:"Premium Ladies Handbag"
-},
-
-{
-    id:9,
-    name:"Gents Shirt",
-    price:899,
-    category:"gents",
-    image:"images/gents/IMG-20260628-WA0026.jpg",
-    description:"Stylish Gents Shirt"
-}
-
-];
