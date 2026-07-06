0b7070c043b65c6bfd07cc6c39ca06d4181d90f3
apnapinddigital2026 <razsekhon5@gmail.com>
2026-07-06 22:50:50 +0530

    Update product.js

--- product.js
+++ product.js
@@ -13,7 +13,86 @@ const products = [
     image: "images/logo/women-suits/IMG-20260628-WA0013.jpg",
     description: "Latest Ladies Suit"
 }
-
+{
+    id: 1,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0013.jpg",
+    description: "Latest Ladies Suit"
+},
+{
+    id: 2,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0025.jpg",
+    description: "Latest Ladies Suit"
+},
+{
+    id: 3,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0030.jpg",
+    description: "Latest Ladies Suit"
+},
+{
+    id: 4,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0033.jpg",
+    description: "Latest Ladies Suit"
+},
+{
+    id: 5,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0035.jpg",
+    description: "Latest Ladies Suit"
+},
+{
+    id: 6,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0036.jpg",
+    description: "Latest Ladies Suit"
+},
+{
+    id: 7,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0037.jpg",
+    description: "Latest Ladies Suit"
+},
+{
+    id: 8,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0038.jpg",
+    description: "Latest Ladies Suit"
+},
+{
+    id: 9,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0039.jpg",
+    description: "Latest Ladies Suit"
+},
+{
+    id: 10,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0040.jpg",
+    description: "Latest Ladies Suit"
+       }
 ];
 
    
