b4c14ff6e54e7307d8a5e2860fcc4f4728ba835f
apnapinddigital2026 <razsekhon5@gmail.com>
2026-07-06 22:15:10 +0530

    Update product.js

--- product.js
+++ product.js
@@ -5,5 +5,15 @@
 
 const products = [
 
-    
+{
+    id: 1,
+    name: "Ladies Suit",
+    price: 1499,
+    category: "ladies",
+    image: "images/logo/women-suits/IMG-20260628-WA0013.jpg",
+    description: "Latest Ladies Suit"
+}
+
 ];
+
+   
