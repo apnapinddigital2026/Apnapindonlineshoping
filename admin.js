69ad1db07451f1bcd41f625c72473044076d9210
apnapinddigital2026 <razsekhon5@gmail.com>
2026-07-11 13:51:16 +0530

    Add product management functionality
    Implemented product addition functionality with local storage.

--- admin.js
+++ admin.js
@@ -1,1 +1,43 @@
+// Load Products
+let products = JSON.parse(localStorage.getItem("products")) || [];
 
+// Save Products
+function saveProducts() {
+    localStorage.setItem("products", JSON.stringify(products));
+    alert("✅ Product Saved Successfully");
+}
+
+// Add Product
+function addProduct() {
+
+    let product = {
+        id: Date.now(),
+
+        code: document.getElementById("code").value,
+
+        name: document.getElementById("name").value,
+
+        category: document.getElementById("category").value,
+
+        color: document.getElementById("color").value,
+
+        size: document.getElementById("size").value,
+
+        oldPrice: Number(document.getElementById("oldPrice").value),
+
+        price: Number(document.getElementById("price").value),
+
+        offer: document.getElementById("offer").value,
+
+        image: document.getElementById("image").value,
+
+        description: document.getElementById("description").value,
+
+        stock: "In Stock"
+    };
+
+    products.push(product);
+
+    saveProducts();
+
+}
